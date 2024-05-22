2cb84180a493dda598336c32619cce3e63958fe6
ewdijf1234 <jjihye0609@naver.com>
2024-05-22 21:33:44 +0900

    test

--- petmein/src/main/petmein/src/App.js
+++ petmein/src/main/petmein/src/App.js
@@ -1,32 +1,14 @@
-// import TopNav from './nav/top-nav';
-// import "./css_doh/App.css"
-import MeongsaengTop from './js_jiho/meongsaeng-top';
-import BottomNav from './nav/bottom-nav';
-import Meongsaeng from './js_jiho/meongsaeng';
-import MyPage from './js_jiho/mypage';
-import MeongsaengContent from './js_jiho/meongsaengContent';
-import SaveBoardPage from './js_jiho/saveboardpage';
-import Following from './js_jiho/following';
-import Follower from './js_jiho/follower';
-
 import "./App.css"
+import Test from "./js_jihye/test";
+import BottomNav from "./nav/bottom-nav";
+import TopNav from "./nav/top-nav";
 
 function App() {
   return (
     <div className="App">
-      {/* 마이페이지, 마이페이지 내 글
-      <MyPage></MyPage>
-      <MeongsaengContent></MeongsaengContent> */}
-      {/* 멍생 페이지 (검색창, 태그박스, 콘텐츠)
-      <MeongsaengTop></MeongsaengTop>
-      <Meongsaeng></Meongsaeng> */}
-      {/* 저장한 게시물
-      <SaveBoardPage></SaveBoardPage> */}
-       {/* 팔로잉, 팔로워
-       <Following></Following>
-       <Follower></Follower> */}
+      <TopNav></TopNav>
       <BottomNav></BottomNav>
-      </div>
+    </div>
 
   );
 }
---
+++ petmein/src/main/petmein/src/js_jihye/test.js
@@ -0,0 +1,10 @@
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function Test() {
+    return (
+        <>
+        <FontAwesomeIcon icon={faHouse} style={{color: "#e4d7ca",}} />
+        </>
+    )
+}
